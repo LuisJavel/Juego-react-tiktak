c58168f1e34899e411b79002b8cbb91d1d0ee554
LMsqz <luisfmasaquiza@hotmail.com>
2024-06-28 13:37:43 +0200

    deploy en gitpage

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://luisjavel.github.io/juego-react-tiktak'
 })
